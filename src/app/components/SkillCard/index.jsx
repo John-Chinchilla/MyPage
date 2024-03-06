26dc99c66bc657cdf6874f43b8750a075124fd17
John Rojas Chinchilla <john.rojas.chinchilla@gmail.com>
2024-03-06 11:15:18 -0600

    ImportIcons File Name Fixed

--- src/app/components/SkillCard/index.jsx
+++ src/app/components/SkillCard/index.jsx
@@ -1,5 +1,5 @@
 "use client"
-import ImportIcons from "@/app/functions/ImportIcons.jsx";
+import ImportIcons from "@/app/functions/ImportIcons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 
